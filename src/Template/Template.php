ffe384c7ba41caf554af53c9224308640e946cc4
Dez <dez@dev-id.net>
2016-09-04 13:22:49 +0300

    fixes

--- src/Template/Template.php
+++ src/Template/Template.php
@@ -12,7 +12,7 @@ use Dez\Template\Extensions\ExtensionDefault;
  * Class Template
  * @package Dez\Template
  */
-class Template {
+class Template implements TemplateInterface {
 
     /**
      * @var DataStorage
